6139bb957669a35f865975fb905d719eff0bbc03
tahz <tahdn4@gmail.com>
2020-06-15 17:24:55 +0700

    acces edit

--- src/app/providers/access-providers.ts
+++ src/app/providers/access-providers.ts
@@ -13,4 +13,22 @@ export class AccessProviders{
     ){}
 
     
+//   getSingin(usr: string, pss: string) {
+//     let headers = new Headers({ 'Content-Type': 'application/json' });
+//    // let options = new RequestOptions({ headers: headers });
+
+//     return new Promise((resolve, reject) => {
+//       this.http.get(this.urlh + '/MLogin?usr=' + usr + '&pss=' + pss)
+//         .toPromise()
+//         .then((response) => {
+//         //  console.log('API Response : ', response.json());
+//           resolve(response.json());
+//         })
+//         .catch((error) => {
+//          // console.error('API Error : ', JSON.stringify(error));
+//           reject(error.json());
+//         });
+//     });
+//   }
+
 } 
